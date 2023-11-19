b855d158122fa415da8e21a8e9cbdeab108477f2
Giuliano Faustini <giuliperus@gmail.com>
2023-11-19 08:23:29 +0200

    8.1: The number of books and authors

--- index.js
+++ index.js
@@ -93,19 +93,31 @@ let books = [
   },
 ]
 
-/*
-  you can remove the placeholder query once your first one has been implemented 
-*/
 
 const typeDefs = `
-  type Query {
-    dummy: Int
+
+type Book {
+    title: String!
+    published: Int
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+  }
+type Query {
+    bookCount: Int!
+    authorCount: Int!
   }
 `
 
 const resolvers = {
   Query: {
-    dummy: () => 0
+    bookCount: () => books.length,
+    authorCount: () => authors.length,
   }
 }
 
