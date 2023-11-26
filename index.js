182ade7984e7de0ff1b5caf73d8887ecf6d77bab
Giuliano Faustini <giuliperus@gmail.com>
2023-11-26 15:58:39 +0200

    8.16 user and logging in

--- index.js
+++ index.js
@@ -3,23 +3,27 @@ const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
 const { v1: uuid } = require("uuid");
 const mongoose = require("mongoose");
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
 const Author = require("./models/Author");
 const Book = require("./models/Book");
+const User = require("./models/User");
+
+const jwt = require("jsonwebtoken");
 const { GraphQLError } = require("graphql");
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-console.log('connecting to', MONGODB_URI)
+console.log("connecting to", MONGODB_URI);
 
-mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('connected to MongoDB')
+    console.log("connected to MongoDB");
   })
   .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
-  })
+    console.log("error connection to MongoDB:", error.message);
+  });
 
 let authors = [
   {
@@ -114,6 +118,15 @@ let books = [
 ];
 
 const typeDefs = `
+type User {
+  username: String!
+  favoriteGenre: String!
+  id: ID!
+}
+
+type Token {
+  value: String!
+}
 
 type Book {
     title: String!
@@ -133,106 +146,194 @@ type Query {
     allBooks(author: String, genres: [String]): [Book!]!
     authorCount: Int!
     allAuthors: [Author!]!
+    me: User
   }
   type Mutation {
+    createUser(
+      username: String!
+      favoriteGenre: String!
+    ): User
+  
+    login(
+      username: String!
+      password: String!
+    ): Token
+  
     addBook(
       title: String!
       published: Int
       author: String!
       genres: [String!]!
     ): Book!
+    
     addAuthor(
       name: String!
     ): Author
+  
     editAuthor(
-        name: String!
-        setBornTo: Int!
-        ) : Author
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `;
 
 const resolvers = {
   Query: {
-   
     bookCount: async () => Book.collection.countDocuments(),
-    allBooks: async (root, args ) => {
+
+    allBooks: async (root, args) => {
       if (args.author) {
-        const author =  await Author.findOne({ name: args.author });
+        const author = await Author.findOne({ name: args.author });
         if (author) {
           return Book.find({ author: author.id });
         }
-      } 
+      }
       if (args.genres) {
         return Book.find({ genres: { $in: args.genres } });
       }
       return Book.find({});
     },
-    authorCount: async () => Author.collection.countDocuments(),
-    allAuthors: async (root, args) => {
-     return Author.find({});
+
+    authorCount: async () => {
+      const count = await Author.collection.countDocuments();
+      return count;
+    },
+
+    allAuthors: async () => {
+      const authors = await Author.find({});
+      return Promise.all(authors.map(async author => {
+        const bookCount = await Book.countDocuments({ author: author.id });
+        return {
+          ...author.toObject(),
+          bookCount,
+          id: author.id.toString(),
+        };
+      }));
+    },
+
+    me: (root, args, context) => {
+      return context.currentUser;
     },
   },
+
   Mutation: {
-    addAuthor: async (root, args) => {
-      if (args.name.length < 3) {
-        throw new GraphQLError('Author name must be at least 3 characters long', {
+    createUser: async (root, args) => {
+      const user = new User({
+        username: args.username,
+        favoriteGenre: args.favoriteGenre,
+      });
+      try {
+        await user.save();
+        return user;
+      } catch (error) {
+        throw new GraphQLError("User validation failed", {
           extensions: {
-            code: 'BAD_USER_INPUT',
+            code: "BAD_USER_INPUT",
             invalidArgs: args,
+            validationErrors: error.errors,
           },
         });
       }
-  
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username });
+      if (!user || args.password !== "secret") {
+        throw new GraphQLError("Wrong credentials", {
+          extensions: {
+            code: "UNAUTHENTICATED",
+          },
+        });
+      }
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      };
+      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) };
+    },
+
+    addAuthor: async (root, args, context) => {
+      if (args.name.length < 3) {
+        throw new GraphQLError(
+          "Author name must be at least 3 characters long",
+          {
+            extensions: {
+              code: "BAD_USER_INPUT",
+              invalidArgs: args,
+            },
+          }
+        );
+      }
+
       const author = new Author({ ...args });
+      const currentUser = context.currentUser;
+      if (!currentUser) {
+        throw new GraphQLError("Not authenticated", {
+          extensions: {
+            code: "UNAUTHENTICATED",
+          },
+        });
+      }
       try {
         await author.save();
         return author;
       } catch (error) {
-        if (error.name === 'ValidationError') {
-          throw new GraphQLError('Author validation failed', {
+        if (error.name === "ValidationError") {
+          throw new GraphQLError("Author validation failed", {
             extensions: {
-              code: 'BAD_USER_INPUT',
+              code: "BAD_USER_INPUT",
               invalidArgs: args,
               validationErrors: error.errors,
             },
           });
         } else {
-          throw new GraphQLError('Author failed to save', {
+          throw new GraphQLError("Author failed to save", {
             extensions: {
-              code: 'INTERNAL_SERVER_ERROR',
+              code: "INTERNAL_SERVER_ERROR",
               error: error.message,
             },
           });
         }
       }
     },
-    addBook: async (root, args) => {
-if(args.title.length < 3) {
-  throw new GraphQLError('Book title must be at least 2 characters long', { 
-    extensions: {
-      code: 'BAD_USER_INPUT',
-      invalidArgs: args,
-    },
-  })
-}
+
+    addBook: async (root, args, context) => {
+      if (args.title.length < 3) {
+        throw new GraphQLError(
+          "Book title must be at least 2 characters long",
+          {
+            extensions: {
+              code: "BAD_USER_INPUT",
+              invalidArgs: args,
+            },
+          }
+        );
+      }
       let author = await Author.findOne({ name: args.author });
+      const currentUser = context.currentUser;
+      if (!currentUser) {
+        throw new GraphQLError("Not authenticated", {
+          extensions: {
+            code: "UNAUTHENTICATED",
+          },
+        });
+      }
       if (!author) {
         author = new Author({ name: args.author });
         try {
           await author.save();
         } catch (error) {
-          if (error.name === 'ValidationError') {
-            throw new GraphQLError('Author validation failed', {
+          if (error.name === "ValidationError") {
+            throw new GraphQLError("Author validation failed", {
               extensions: {
-                code: 'BAD_USER_INPUT',
+                code: "BAD_USER_INPUT",
                 invalidArgs: args,
                 validationErrors: error.errors,
               },
             });
           } else {
-            throw new GraphQLError('Author failed to save', {
+            throw new GraphQLError("Author failed to save", {
               extensions: {
-                code: 'INTERNAL_SERVER_ERROR',
+                code: "INTERNAL_SERVER_ERROR",
                 error: error.message,
               },
             });
@@ -242,21 +343,29 @@ if(args.title.length < 3) {
 
       try {
         const book = new Book({ ...args, author: author.id });
+        const currentUser = context.currentUser;
+        if (!currentUser) {
+          throw new GraphQLError("Not authenticated", {
+            extensions: {
+              code: "UNAUTHENTICATED",
+            },
+          });
+        }
         await book.save();
         return book;
       } catch (error) {
-        if (error.name === 'ValidationError') {
-          throw new GraphQLError('Book validation failed', {
+        if (error.name === "ValidationError") {
+          throw new GraphQLError("Book validation failed", {
             extensions: {
-              code: 'BAD_USER_INPUT',
+              code: "BAD_USER_INPUT",
               invalidArgs: args,
               validationErrors: error.errors,
             },
           });
         } else {
-          throw new GraphQLError('Book failed to save', {
+          throw new GraphQLError("Book failed to save", {
             extensions: {
-              code: 'INTERNAL_SERVER_ERROR',
+              code: "INTERNAL_SERVER_ERROR",
               error: error.message,
             },
           });
@@ -266,36 +375,36 @@ if(args.title.length < 3) {
 
     editAuthor: async (root, args) => {
       const author = await Author.findOne({ name: args.name });
-  
+
       if (!author) {
         return null;
       }
-  
+
       try {
         author.born = args.setBornTo;
         await author.save();
         return author;
       } catch (error) {
-        if (error.name === 'ValidationError') {
-          throw new GraphQLError('Author validation failed', {
+        if (error.name === "ValidationError") {
+          throw new GraphQLError("Author validation failed", {
             extensions: {
-              code: 'BAD_USER_INPUT',
+              code: "BAD_USER_INPUT",
               invalidArgs: args,
               validationErrors: error.errors,
             },
           });
         } else {
-          throw new GraphQLError('Saving author failed', {
+          throw new GraphQLError("Saving author failed", {
             extensions: {
-              code: 'INTERNAL_SERVER_ERROR',
+              code: "INTERNAL_SERVER_ERROR",
               error: error.message,
             },
           });
         }
       }
     },
-}
-}
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
@@ -304,6 +413,19 @@ const server = new ApolloServer({
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
+  context: async ({ req, res }) => {
+    const auth = req ? req.headers.authorization : null;
+    if (auth && auth.startsWith("Bearer ")) {
+      const decodedToken = jwt.verify(
+        auth.substring(7),
+        process.env.JWT_SECRET
+      );
+      const currentUser = await User.findById(decodedToken.id).populate(
+        "favoriteGenre"
+      );
+      return { currentUser };
+    }
+  },
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
