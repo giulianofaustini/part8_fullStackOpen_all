6d9ff09ea753ee4d3921e24a60837a652e9d3e4b
Giuliano Faustini <giuliperus@gmail.com>
2023-11-28 14:28:17 +0200

    8.19 Books by genre, part 1

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,30 +1,48 @@
+
+import React, { useState } from 'react';
+
 const Books = ({ books }) => {
+  const [selectedGenre, setSelectedGenre] = useState(null);
+
+
+  const getGenres = (books) => books.flatMap((b) => b.genres);
   
 
-  console.log('from books page to say books', books)
+  const allGenres = getGenres(books)
+  console.log('this is the getGenres to show the flat map:', allGenres);
+  const newGenres = [...new Set(allGenres)];
+
+  const handleGenreClick = (genre) => {
+    setSelectedGenre(genre);
+  };
+
+  const filteredBooks = selectedGenre
+    ? books.filter((b) => b.genres.includes(selectedGenre))
+    : books;
 
   return (
     <div>
-      <h2>books</h2>
+      <h2>Books</h2>
+      <div>
+        {newGenres.map((genre) => (
+          <button key={genre} onClick={() => handleGenreClick(genre)}>
+            {genre}
+          </button>
+        ))}
+        <div>
+        <button onClick={() => setSelectedGenre(null)}>Show All</button>
+        </div>
+      </div>
 
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>author</th>
-            <th>published</th>
-          </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <ul style={{ listStyleType: 'none' }}>
+        {filteredBooks.map((b) => (
+          <li key={b.title}>
+            {b.title} by {b.author.name}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Books
+export default Books;
