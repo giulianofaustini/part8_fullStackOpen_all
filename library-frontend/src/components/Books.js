84cdcc941e2b122b3898677e82bd268ca8195aa7
Giuliano Faustini <giuliperus@gmail.com>
2023-11-29 07:40:16 +0200

    8.21 books by genre with GraphQL 2

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,22 +1,36 @@
 import React, { useState } from 'react';
+import { BOOKS_IN_GENRE } from '../queries'
+import { useQuery } from '@apollo/client';
 
 
 const Books = ({ books }) => {
   const [selectedGenre, setSelectedGenre] = useState(null);
 
 
-  const getGenres = (books) => books.flatMap((b) => b.genres);
+
+  const booksInGenreQuery = useQuery( BOOKS_IN_GENRE, {
+    onError: (error) => {
+      console.error('Error fetching books in genre query', error.message)
+    },
+    variables: { genre: selectedGenre },
+  })
+
+
   
+
+  console.log('This is books by genres in Books component', booksInGenreQuery)
+
+
+  const getGenres = (books) => books.flatMap((b) => b.genres);
 
   const allGenres = getGenres(books)
   console.log('this is the getGenres to show the flat map:', allGenres);
   const newGenres = [...new Set(allGenres)];
 
+
   const handleGenreClick = (genre) => {
     setSelectedGenre(genre);
   };
-
-
 
   const filteredBooks = selectedGenre ? books.filter((b) => b.genres.includes(selectedGenre)) : books;
 
@@ -48,3 +62,54 @@ const Books = ({ books }) => {
 
 export default Books;
 
+
+
+// import React, { useState } from 'react';
+
+
+// const Books = ({ books }) => {
+//   const [selectedGenre, setSelectedGenre] = useState(null);
+
+
+//   const getGenres = (books) => books.flatMap((b) => b.genres);
+  
+
+//   const allGenres = getGenres(books)
+//   console.log('this is the getGenres to show the flat map:', allGenres);
+//   const newGenres = [...new Set(allGenres)];
+
+//   const handleGenreClick = (genre) => {
+//     setSelectedGenre(genre);
+//   };
+
+
+
+//   const filteredBooks = selectedGenre ? books.filter((b) => b.genres.includes(selectedGenre)) : books;
+
+
+//   return (
+//     <div>
+//       <h2>Books</h2>
+//       <div>
+//         {newGenres.map((genre) => (
+//           <button key={genre} onClick={() => handleGenreClick(genre)}>
+//             {genre}
+//           </button>
+//         ))}
+//         <div>
+//           <button onClick={() => setSelectedGenre(null)}>Show All</button>
+//         </div>
+//       </div>
+
+//       <ul style={{ listStyleType: 'none' }}>
+//         {filteredBooks.map((b) => (
+//           <li key={b.title}>
+//             {b.title} by {b.author.name}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default Books;
